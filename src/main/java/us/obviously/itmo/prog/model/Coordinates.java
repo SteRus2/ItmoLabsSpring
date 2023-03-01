7c0d256463a214c1c8a2a068ebd941ec11df769c
SteRus2 <Stepannigamatullin228654@gmail.com>
2023-03-01 12:00:06 +0300

    [861mdgq8d] feat: change model

--- src/main/java/us/obviously/itmo/prog/model/Coordinates.java
+++ src/main/java/us/obviously/itmo/prog/model/Coordinates.java
@@ -3,4 +3,9 @@ package us.obviously.itmo.prog.model;
 public class Coordinates {
     private Long x; //Поле не может быть null
     private float y; //Значение поля должно быть больше -373
+    public Coordinates(){}
+    public Coordinates(Long x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 }
