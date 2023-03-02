5c3e0dd59377afcf21a6e32b945b1bb36d8fb69d
Danilov Timofey <volinadtim@gmail.com>
2023-03-02 07:19:28 +0300

    [1235] replace if greater command

--- src/main/java/us/obviously/itmo/prog/commands/ReplaceIfGreaterCommand.java
+++ src/main/java/us/obviously/itmo/prog/commands/ReplaceIfGreaterCommand.java
@@ -1,6 +1,10 @@
 package us.obviously.itmo.prog.commands;
 
+import us.obviously.itmo.prog.console.ConsoleColors;
+import us.obviously.itmo.prog.exceptions.NoSuchIdException;
+import us.obviously.itmo.prog.forms.StudyGroupForm;
 import us.obviously.itmo.prog.manager.Management;
+import us.obviously.itmo.prog.model.StudyGroup;
 
 import java.util.HashMap;
 
@@ -16,7 +20,18 @@ public class ReplaceIfGreaterCommand extends AbstractCommand {
      */
     @Override
     public void execute(HashMap<String, String> args) {
-
+        var studyGroupForm = new StudyGroupForm(this.manager);
+        var key = args.get("key");
+        studyGroupForm.update(key);
+        StudyGroup studyGroup = studyGroupForm.build();
+        try {
+            this.manager.getDataCollection().replaceIfGreater(studyGroup.getId(), studyGroup);
+            System.out.println(ConsoleColors.BLUE +
+                    "Если это было необходимо, studyGroup обновлён под id %s".formatted(studyGroup.getId()) +
+                    ConsoleColors.RESET);
+        } catch (NoSuchIdException e) {
+            System.out.println(ConsoleColors.RED + "Ошибка при сохранении: " + e.getMessage() + ConsoleColors.RESET);
+        }
     }
 
 }
