639339b7903d7c2037039c206180b34b19e44180
Danilov Timofey <volinadtim@gmail.com>
2023-03-16 10:34:03 +0300

    [hotfix] hotfix

--- src/main/java/us/obviously/itmo/prog/forms/DateForm.java
+++ src/main/java/us/obviously/itmo/prog/forms/DateForm.java
@@ -1,5 +1,6 @@
 package us.obviously.itmo.prog.forms;
 
+import us.obviously.itmo.prog.console.Messages;
 import us.obviously.itmo.prog.exceptions.FormInterruptException;
 import us.obviously.itmo.prog.exceptions.IncorrectValueException;
 import us.obviously.itmo.prog.fields.IntegerFormField;
@@ -32,7 +33,12 @@ public class DateForm extends Form<ZonedDateTime> {
         new IntegerFormField(manager, "month", this::setMonth).run();
         new IntegerFormField(manager, "day", this::setDay).run();
         new IntegerFormField(manager, "offset hours", this::setHours, false, 0, null).run();
-        new IntegerFormField(manager, "offset minutes", this::setMinutes, false, 0, null).run();
+        if (this.hours * this.hours < 18 * 18) {
+            new IntegerFormField(manager, "offset minutes", this::setMinutes, false, 0, null).run();
+        } else {
+            Messages.printStatement("~grМинуты установлены в 00 по умолчанию, т.к. %s часов типа~=".formatted(this.hours));
+            this.minutes = 0;
+        }
     }
 
     /**
@@ -84,8 +90,7 @@ public class DateForm extends Form<ZonedDateTime> {
             this.hours = 0;
             return;
         }
-        if (value > 23) throw new IncorrectValueException("В дне 24 часа.");
-        if (value < 0) throw new IncorrectValueException("В дне 24 натуральных или ноль часа.");
+        if (value > 18 || value < -18) throw new IncorrectValueException("В смещении может быть от -18:00 до +18:00");
         this.hours = value;
     }
 
@@ -110,6 +115,9 @@ public class DateForm extends Form<ZonedDateTime> {
      * @return Готовая Дата
      */
     public ZonedDateTime build() {
-        return ZonedDateTime.parse("%04d-%02d-%02dT00:00:00+%02d:%02d".formatted(year, month, day, hours, minutes));
+        if (hours >= 0) {
+            return ZonedDateTime.parse("%04d-%02d-%02dT00:00:00+%02d:%02d".formatted(year, month, day, hours, minutes));
+        }
+        return ZonedDateTime.parse("%04d-%02d-%02dT00:00:00%02d:%02d".formatted(year, month, day, hours, minutes));
     }
 }
