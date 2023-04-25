2ce4fc010fecb1fc41ffa051cc8a6e7819d63be0
Danilov Timofey <volinadtim@gmail.com>
2023-04-25 23:07:47 +0300

    actions and so on on on щт2

--- src/main/java/us/obviously/itmo/prog/common/serializers/Serializer.java
+++ src/main/java/us/obviously/itmo/prog/common/serializers/Serializer.java
@@ -2,16 +2,22 @@ package us.obviously.itmo.prog.common.serializers;
 
 import us.obviously.itmo.prog.client.exceptions.IncorrectValueException;
 import us.obviously.itmo.prog.server.exceptions.CantParseDataException;
+import us.obviously.itmo.prog.server.exceptions.FailedToDumpsEx;
 import us.obviously.itmo.prog.server.exceptions.IncorrectValuesTypeException;
 
 import java.io.*;
 
 public interface Serializer<T> {
-    default String serialize(T object) throws IOException {
+    default String serialize(T object) throws FailedToDumpsEx {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
-        oos.writeObject(object);
-        oos.close();
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            oos.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return baos.toString();
     }
 
