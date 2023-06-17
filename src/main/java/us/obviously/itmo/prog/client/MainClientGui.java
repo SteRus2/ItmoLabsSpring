df9f9c50734b7a1d43f308c0014beeab4f2d2399
Danilov Timofey <volinadtim@gmail.com>
2023-06-17 20:41:08 +0300

    chore: remove javafx from the server and client module

--- src/main/java/us/obviously/itmo/prog/client/MainClientGui.java
+++
@@ -1,9 +0,0 @@
-package us.obviously.itmo.prog.client;
-
-import javafx.application.Application;
-
-public class MainClientGui {
-    public static void main(String[] args) {
-        Application.launch(MainClient.class, args);
-    }
-}
