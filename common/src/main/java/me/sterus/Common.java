399e62937ae53b34b526fa96ee2effa60ca5354e
Danilov Timofey <volinadtim@gmail.com>
2023-06-19 12:01:45 +0300

    chore: project settings

--- common/src/main/java/me/sterus/Common.java
+++
@@ -1,9 +0,0 @@
-package me.sterus;
-
-public class Common {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
-    public static final String HELLO = "sa";
-}
